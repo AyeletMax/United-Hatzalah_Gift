6599eaceb5b39d254aa73269ba7149551a1aaf73
AyeletMax <Hand4d20@mbjcomp.org.il>
2025-10-05 05:54:19 +0300

    Fix all merge conflicts - keep routing, filtering, and responsive

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-=======
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Nav from './components/Nav.jsx'
->>>>>>> 7c882dbde7a63e5bc87bd806a0d5e11f08602b24
 
 import App from "./App.jsx";
 import Nav from "./components/Nav.jsx";
@@ -17,7 +9,6 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-<<<<<<< HEAD
     <BrowserRouter>
       <ProductsProvider>
         <Nav />
@@ -26,9 +17,3 @@ createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </StrictMode>
 );
-=======
-    <App />
-    <Nav />
-  </StrictMode>,
-)
->>>>>>> 7c882dbde7a63e5bc87bd806a0d5e11f08602b24
