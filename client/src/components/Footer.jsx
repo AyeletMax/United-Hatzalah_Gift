57545f1277bff225a7ee15ad16b994772ebd8ea0
AyeletMax <Hand4d20@mbjcomp.org.il>
2025-10-26 01:29:03 +0200

    שינוי הכיתוב של האתר נבנה ע"י לאנגלית

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>האתר נבנה על ידי צפורה הימל ואיילת מקסימוב</p>
+        <p>Website built by Tzipora Himel & Ayelet Maximov</p>
         <div className="footer-emails">
           <a href="mailto:tzipora.himel@gmail.com">tzipora.himel@gmail.com</a>
           <span> | </span>
